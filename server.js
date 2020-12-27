18c9492eb3d583c73e6e2244d96b8f05a1a06270
Andy Chen <chenyangpoppin@hotmail.com>
2020-12-27 18:17:11 +0800

    feat: add authenticate jwt token middleware code logic

--- server.js
+++ server.js
@@ -19,8 +19,36 @@ const posts = [
   }
 ];
 
-app.get('/posts', (req, res) => {
-  res.json(posts);
+/**
+ * A simple middleware used to retrieve jwt token and verify it, a http header with authorization looks like this:
+ * Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSmFja2llIiwiaWF0IjoxNjA5MDYzNzAzfQ.H_DdgU1ymVpHFga1mOeq0ppcCPKajjvQodjaDZh86v4
+ */
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) {
+    // 401 unauthorized
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      // 403 forbidden
+      return res.sendStatus(403);
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+app.get('/posts', authenticateToken, (req, res) => {
+  res.json(posts.filter
+    (
+      post => post.username === req.user.name
+    )
+  );
 });
 
 /**
